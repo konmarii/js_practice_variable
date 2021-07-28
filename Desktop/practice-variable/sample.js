e6a9005cc0c5abbdb0fc9f21bd42b85ac160e2bf
konmarii <chankonworld@gmail.com>
2021-07-28 14:23:23 +0900

    [update]変数宣言

---
+++ Desktop/practice-variable/sample.js
@@ -0,0 +1,36 @@
+//varによる再宣言
+//var nikname = 'taro'
+//console.log(nikname)
+//var nikname = 'ichiro'
+//console.log(nikname)
+//success
+
+//let nikname = 'taro'
+//console.log(nikname)
+//let nikname = 'ichiro'
+//console.log(nikname
+//error
+
+//const nikname = 'taro'
+//console.log(nikname)
+//const nikname = 'ichiro'
+//console.log(nikname)
+//error
+
+//var nikname = 'taro'
+//console.log(nikname)
+//nikname = 'jiro'
+//console.log(nikname)
+//success
+
+//let nikname = 'taro'
+//console.log(nikname)
+//nikname = 'jiro'
+//console.log(nikname)
+//success
+
+//const nikname = 'taro'
+//console.log(nikname)
+//nikname = 'jiro'
+//console.log(nikname)
+//error
